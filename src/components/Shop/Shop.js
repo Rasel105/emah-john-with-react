8cecf74ad7121e38a0e1e44ffc194a56cea88219
Rasel <azad.is.rasel@gmail.com>
2022-03-22 16:25:52 +0600

    product added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+const Shop = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    const handleAddToCart = (product) => {
+        console.log(product)
+    }
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h3>Order summary</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
